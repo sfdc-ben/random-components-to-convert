6699e832b229b87a9c9f50743f65da2ae78f7490
sfdc-ben <55468295+sfdc-ben@users.noreply.github.com>
2021-10-03 17:58:24 -0700

    AC Update

--- force-app/main/default/lwc/acStatusHeader/acStatusHeader.js
+++ force-app/main/default/lwc/acStatusHeader/acStatusHeader.js
@@ -1,3 +1,20 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class AcStatusHeader extends LightningElement {}
+export default class AcStatusHeader extends LightningElement {
+
+    @track isModalOpen = false;
+    openModal() {
+        // to open modal set isModalOpen tarck value as true
+        this.isModalOpen = true;
+    }
+    closeModal() {
+        // to close modal set isModalOpen tarck value as false
+        this.isModalOpen = false;
+    }
+    submitDetails() {
+        // to close modal set isModalOpen tarck value as false
+        //Add your code to call apex method or do some processing
+        this.isModalOpen = false;
+    }
+    
+}
--- force-app/main/default/lwc/acTravelerDetails/acTravelerDetails.js
+++ force-app/main/default/lwc/acTravelerDetails/acTravelerDetails.js
@@ -1,3 +1,45 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class AcTravelerDetails extends LightningElement {}
+export default class AcTravelerDetails extends LightningElement {
+    @api color = 'black'
+    @api headColor = 'black'
+    @api deptFlightOrigin
+    @api deptFlightDest
+    @api deptFlightDate
+    @api deptFlightItin
+    @api deptFlightDeptTime
+    @api deptFlightArrTime
+    @api deptFlightDuration
+    @api deptFlightStopOver
+    @api deptFlightStopOverDuration
+    @api deptFlightLeg1Detail
+    @api deptFlightLeg2Detail
+
+    @api arrFlightOrigin
+    @api arrFlightDest
+    @api arrFlightDate
+    @api arrFlightItin
+    @api arrFlightDeptTime
+    @api arrFlightArrTime
+    @api arrFlightDuration
+    @api arrFlightStopOver
+    @api arrFlightStopOverDuration
+    @api arrFlightLeg1Detail
+    @api arrFlightLeg2Detail
+
+    @api householdImage
+
+    get icnStyle() {
+        return `
+            --sds-c-icon-color-foreground-default: ${this.color};
+            --sds-c-icon-color-foreground: ${this.color};
+            --sds-c-icon-color-background: white;
+            `
+    }
+
+    get headStyle() {
+        return `
+            background: ${this.headColor};
+            `
+    }
+}
